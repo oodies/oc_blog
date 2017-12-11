bfd7d7389df00e630b7c2ef39b9efb203003dbf3
schomy <sebastien.chomy@gmail.com>
2017-12-11 15:37:28 +0100

    #3 Data persist to database - Add a abstract class to table gateway children's classes

---
+++ lib/Db/AbstractTableGateway.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * This file is part of oc_blog project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2017/12
+ */
+
+namespace Lib\Db;
+
+use Lib\Db\Adapter\AbstractDB;
+use Lib\Registry;
+
+/**
+ * Class AbstractTableGateway
+ * @package Lib\Db
+ */
+class AbstractTableGateway
+{
+    /** *******************************
+     * PROPERTIES
+     * ****************************** */
+
+    /**
+     * @var AbstractDB null
+     */
+    public $db = null;
+
+
+    /** *******************************
+     * METHODS
+     * ****************************** */
+
+    /**
+     * AbstractTableGateway constructor.
+     *
+     * Children's classes must have a 'tableName' property.
+     * Children's classes must have a 'primary' property.
+     */
+    public final function __construct()
+    {
+        if (!isset($this->tableName)) {
+            throw new \LogicException(get_class($this) . ' must have a $tableName property');
+        }
+
+        if (!isset($this->primary)) {
+            throw new \LogicException(get_class($this) . ' must have a $primary property');
+        }
+    }
+
+    /**
+     * Fectches rows by primary key
+     *
+     * @param $id
+     *
+     * @return false|array
+     */
+    public function find($id)
+    {
+        $sql = "SELECT * FROM " . $this->tableName .
+            " WHERE " . $this->primary . "= :id";
+
+        /** @var \PDOStatement $statement */
+        $statement = $this->getAdapter()->getConnection()->prepare($sql);
+        $statement->execute([':id' => $id]);
+
+        return $statement->fetch();
+    }
+
+    /**
+     * @return AbstractDB
+     */
+    public function getAdapter()
+    {
+        if ($this->db === null) {
+            self::setAdapter(Registry::get('db'));
+        }
+        return $this->db;
+    }
+
+    /**
+     * Set dbAdapter property
+     */
+    public function setAdapter($db)
+    {
+        $this->db = $db;
+        return $this;
+    }
+
+    /**
+     * Find all row
+     *
+     * @return array
+     */
+    public function findAll()
+    {
+        $sql = "SELECT * FROM " . $this->tableName;
+        /** @var \PDOStatement $statement */
+        $statement = $this->getAdapter()->getConnection()->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+
+    /**
+     * Insert a new row
+     *
+     * @param array $data
+     *
+     * @throws \Exception
+     */
+    public function insert(array $data)
+    {
+
+        $sql = 'INSERT INTO ';
+        $sql .= $this->tableName . '(';
+        $sql .= rtrim(implode(',', array_keys($data)), ',');
+
+        $sql .= ') VALUES(:';
+        $sql .= rtrim(implode(',:', array_keys($data)), ',:');
+        $sql .= ')';
+
+        $bind = [];
+        foreach ($data as $field => $value) {
+            // Ici un quoteInto
+            $bind[$field] = $value;
+        }
+
+        $statement = $this->getAdapter()->getConnection()->prepare($sql);
+        $statement->execute($bind);
+    }
+
+
+    /**
+     * Updates existing rows
+     *
+     * @param array $data
+     * @param       $where
+     */
+    public function update(array $data, $where)
+    {
+
+    }
+
+}
