02661781d41acbd4926c1f3348fd77e3a33d1c97
schomy <sebastien.chomy@gmail.com>
2017-12-12 10:19:31 +0100

    #3 Data persist to database - [Model Domain] Add a blogpost aggregate

---
+++ src/blogpost/Domain/Model/PostAggregate.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * This file is part of oc_blog project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2017/12
+ */
+
+namespace Blogpost\Domain\Model;
+
+use Blogpost\Domain\ValueObject\PostID;
+use User\Domain\Model\Person;
+
+/**
+ * Class PostAggregate
+ * @package Blogpost\Domain\Model
+ */
+class PostAggregate extends Post
+{
+    /** *******************************
+     *  ATTRIBUTES
+     */
+
+    /** @var Header $header */
+    protected $header;
+
+    /** @var Body $body */
+    protected $body = null;
+
+    /** @var Blogger $blogger */
+    protected $blogger = null;
+
+
+    /** *******************************
+     *  METHODS
+     */
+
+    /**
+     * PostAggregate constructor.
+     *
+     * @param PostID|null $postID
+     */
+    public function __construct(?PostID $postID = null)
+    {
+        if ($postID === null) {
+            $postID = new PostID();
+        }
+
+        $this->setPostID($postID);
+        $this->header = new Header();
+        $this->header->setPostID($postID);
+        $this->body = new Body();
+        $this->body->setPostID($postID);
+        $this->blogger = new Blogger();
+    }
+
+
+    /** *******************************
+     *  GETTER/SETTER
+     */
+
+    /**
+     * @return Body
+     */
+    public function getBody(): Body
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param Body $body
+     *
+     * @return PostAggregate
+     */
+    public function setBody(Body $body): PostAggregate
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return Person|null
+     */
+    public function getBlogger(): ?Person
+    {
+        return $this->blogger;
+    }
+
+    /**
+     * @param Person $blogger
+     *
+     * @return PostAggregate
+     */
+    public function setBlogger(Person $blogger): PostAggregate
+    {
+        $this->blogger = $blogger;
+        return $this;
+    }
+
+    /**
+     * @return Header
+     */
+    public function getHeader(): Header
+    {
+        return $this->header;
+    }
+
+    /**
+     * @param Header $header
+     *
+     * @return PostAggregate
+     */
+    public function setHeader(Header $header): PostAggregate
+    {
+        $this->header = $header;
+        return $this;
+    }
+}
