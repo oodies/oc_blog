aa7d94e19da6c787f300611f923ea818093e4e9d
schomy <sebastien.chomy@gmail.com>
2017-12-14 16:58:40 +0100

    #8 Comment : Domain - Add value object

---
+++ src/comment/Domain/ValueObject/AuthorID.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of oc_blog project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2017/12
+ */
+
+namespace Comment\Domain\ValueObject;
+
+use User\Domain\ValueObject\UserID;
+
+/**
+ * Class AuthorID
+ * @package Comment\Domain\ValueObject
+ */
+class AuthorID extends UserID
+{
+
+}
