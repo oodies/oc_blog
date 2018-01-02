625bb0636ad5c5fbc54bac1961b820ab8527d858
schomy <sebastien.chomy@gmail.com>
2018-01-02 23:03:06 +0100

    #1 Project structure and initialization - Update index.php for  -- session_start  -- Add environment utils

--- index.php
+++ index.php
@@ -5,11 +5,26 @@
  * Date: 01/12/2017
  * Time: 11:56
  */
+session_start();
 
 defined('ROOT_DIR') || define('ROOT_DIR', __DIR__);
 
 require_once ROOT_DIR . '/vendor/autoload.php';
 
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/configs/.env');
+$env = (getenv('ENV'))??'prod';
+
+if ($env == 'dev') {
+    Debug::enable();
+    ErrorHandler::register();
+    ExceptionHandler::register();
+}
 // Initialization application
 require_once 'Bootstrap.php';
 $bootstrap = new Bootstrap();
